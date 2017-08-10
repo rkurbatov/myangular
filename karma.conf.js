c9d2cf7fe57b6b1b8c18b9c554a18917ffb3886c
Roman Kurbatov <rkurbatov@gmail.com>
2017-08-10 17:20:04 +0300

    initial config

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['browserify', 'jasmine'],
+    files: [
+      'src/**/*.js',
+      'test/**/*.test.js'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['jshint', 'browserify'],
+      'test/**/*.js': ['jshint', 'browserify']
+    },
+    browsers: ['PhantomJS'],
+    browserify: {
+      debug: true
+    }
+  });
+};
