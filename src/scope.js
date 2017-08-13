f4b6e92bfc659d41e4b62ab239e0151a4d2ab981
Roman Kurbatov <rkurbatov@gmail.com>
2017-08-13 11:17:08 +0300

    converts scope into class

--- src/scope.js
+++ src/scope.js
@@ -1,63 +1,69 @@
 import { isEqual, cloneDeep } from 'lodash'
 
-const Scope = function () {
-  this.$$watchers = []
-  this.$$lastDirtyWatch = null // Required for digest loop optimization
-}
-
-// Function is a reference value, as it equals only to itself.
+// Symbol is a reference value, as it equals only to itself.
 // It is set as an initial watch value to distinct it from undefined
-const initWatchVal = () => {}
+const INIT_WATCH_VALUE = Symbol('initial watch value')
 
-Scope.prototype.$watch = function(watchFn, listenerFn = () => {}, valueEq = false) {
-  const watcher = {
-    watchFn,
-    listenerFn,
-    valueEq,
-    last: initWatchVal
+class Scope {
+
+  constructor () {
+    this.$$watchers = []
+    this.$$lastDirtyWatch = null // Required for digest loop optimization
   }
-  this.$$watchers.push(watcher)
-}
 
-Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
-  if (valueEq) {
-    return isEqual(newValue, oldValue)
-  } else {
-    return (Number.isNaN(newValue) && Number.isNaN(oldValue)) || newValue === oldValue
-  }
-}
-
-Scope.prototype.$$digestOnce = function () {
-  let dirty
-  // Array#every allows short circuiting by return of false values
-  this.$$watchers.every(watcher => {
-    const newValue = watcher.watchFn(this)
-    const oldValue = watcher.last
-    if (!this.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-      this.$$lastDirtyWatch = watcher
-      // We're preventing leakage of the initWatchVal out of scope
-      // and sending newValue as on oldValue for the first digest
-      const oldValueToPass = oldValue === initWatchVal ? newValue : oldValue
-      watcher.listenerFn(newValue, oldValueToPass, this)
-      watcher.last = watcher.valueEq ? cloneDeep(newValue) : newValue
-      dirty = true
-    } else if (watcher === this.$$lastDirtyWatch) {
-      // No need to run remaining watchers as we've just run last dirty of them
-      return false
+  $watch (watchFn, listenerFn = () => {}, valueEq = false) {
+    const watcher = {
+      watchFn,
+      listenerFn,
+      valueEq,
+      last: INIT_WATCH_VALUE
     }
-    return true
-  })
-  return dirty
-}
+    this.$$watchers.push(watcher)
+  }
 
-Scope.prototype.$digest = function () {
-  let dirty
-  let TTL = 10
-  this.$$lastDirtyWatch = null
-  do {
-    dirty = this.$$digestOnce()
-    if (dirty && !(TTL--)) throw new Error('Maximum $watch TTL exceeded')
-  } while (dirty)
+
+  static $$areEqual (newValue, oldValue, valueEq) {
+    if (valueEq) {
+      return isEqual(newValue, oldValue)
+    } else {
+      return newValue === oldValue ||
+             (Number.isNaN(newValue) && Number.isNaN(oldValue))
+    }
+  }
+
+  $$digestOnce () {
+    let dirtyFlag = false
+    // Array#every allows short circuiting by return of false values
+    this.$$watchers.every(watcher => {
+      const newValue = watcher.watchFn(this)
+      const oldValue = watcher.last
+      if (!Scope.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+        this.$$lastDirtyWatch = watcher
+        // We're preventing leakage of the initWatchVal out of scope
+        // and sending newValue as on oldValue for the first digest
+        const oldValueToPass = oldValue === INIT_WATCH_VALUE ? newValue : oldValue
+        watcher.listenerFn(newValue, oldValueToPass, this)
+        watcher.last = watcher.valueEq ? cloneDeep(newValue) : newValue
+        dirtyFlag = true
+      } else if (watcher === this.$$lastDirtyWatch) {
+        // No need to run remaining watchers as we've just run last dirty of them
+        return false
+      }
+      return true
+    })
+    return dirtyFlag
+  }
+
+  $digest () {
+    let dirtyFlag
+    let TTL = 10
+    this.$$lastDirtyWatch = null
+    do {
+      dirtyFlag = this.$$digestOnce()
+      if (dirtyFlag && !(TTL--)) throw new Error('Maximum $watch TTL exceeded')
+    } while (dirtyFlag)
+  }
+
 }
 
 export default Scope
