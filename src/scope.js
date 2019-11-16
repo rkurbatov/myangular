832dbb445e182ff1c11d238f3169da5c54c48497
Roman Kurbatov <rkurbatov@gmail.com>
2019-11-17 00:33:18 +0200

    $evalAsync / $applyAsync

--- src/scope.js
+++ src/scope.js
@@ -9,8 +9,15 @@ class Scope {
     this.$$watchers = [];
     this.$$lastDirtyWatch = null; // Required for digest loop optimization
     this.$$asyncQueue = [];
+    this.$$applyAsyncQueue = [];
+    this.$$applyAsyncId = null; // Set if applyAsync timeout has been scheduled
+    this.$$phase = null; // "$digest" | "$apply" | null
   }
 
+  // $watch function attaches watcher to the scope
+  // watchFn - is the function taking the scope as an argument and returning value we should watch
+  // listenerFn - is the function that is called on data change
+  // valueEq — should we use shallow equality check instead of reference check
   $watch(watchFn, listenerFn = () => {}, valueEq = false) {
     const watcher = {
       watchFn,
@@ -73,36 +80,94 @@ class Scope {
     return dirtyFlag;
   }
 
+  // Starts digest cycle
   $digest() {
     let dirtyFlag;
     let TTL = 10;
     this.$$lastDirtyWatch = null;
+    this.$beginPhase("$digest");
+
+    // Immediately flush applyAsync queue
+    if (this.$$applyAsyncId) {
+      clearTimeout(this.$$applyAsyncId);
+      this.$$flushApplyAsync();
+    }
+
     do {
       while (this.$$asyncQueue.length) {
         const asyncTask = this.$$asyncQueue.shift();
         asyncTask.scope.$eval(asyncTask.expression);
       }
       dirtyFlag = this.$$digestOnce();
-      TTL -= 1;
-      if ((dirtyFlag || this.$$asyncQueue.length) && !TTL)
+      TTL--;
+      if ((dirtyFlag || this.$$asyncQueue.length) && !TTL) {
+        this.$clearPhase();
         throw new Error("Maximum $watch TTL exceeded");
+      }
     } while (dirtyFlag || this.$$asyncQueue.length);
+    this.$clearPhase();
   }
 
+  // Executes the code in context of scope
   $eval(expr, locals) {
     return expr(this, locals);
   }
 
+  // Executes the function in context of scope and starts digest.
+  // Used for library integrations.
   $apply(expr) {
     try {
+      this.$beginPhase("$apply");
       return this.$eval(expr);
     } finally {
+      this.$clearPhase();
       this.$digest();
     }
   }
 
+  // Deferred execution of the function but guaranteed during the current digest cycle.
+  // If there is no digest, the one is started.
   $evalAsync(expr) {
+    if (!this.$$phase && !this.$$asyncQueue.length) {
+      setTimeout(() => {
+        if (this.$$asyncQueue.length) {
+          this.$digest();
+        }
+      }, 0);
+    }
     this.$$asyncQueue.push({ scope: this, expression: expr });
+  }
+
+  // Deferred execution of the function but guaranteed during the next digest cycle.
+  // Optimizes quick succession of events running all of them in one digest, should be used
+  // instead of several applies in a row.
+  $applyAsync(expr) {
+    this.$$applyAsyncQueue.push(() => {
+      this.$eval(expr);
+    });
+    if (this.$$applyAsyncId === null) {
+      this.$$applyAsyncId = setTimeout(() => {
+        this.$apply(this.$$flushApplyAsync.bind(this));
+      }, 0);
+    }
+  }
+
+  $$flushApplyAsync() {
+    while (this.$$applyAsyncQueue.length) {
+      this.$$applyAsyncQueue.shift()();
+    }
+    this.$$applyAsyncId = null;
+  }
+
+  $beginPhase(phase) {
+    if (this.$$phase) {
+      throw this.$$phase + " already in progress.";
+    }
+    this.$$phase = phase;
+  }
+
+  $clearPhase() {
+    this.$$phase = null;
   }
 }
 
